8f76ee0047071c46327786f99c0f9e669e20c65b
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-09-12 23:20:09 +0900

    Add for, function

--- main.go
+++ main.go
@@ -10,4 +10,31 @@ func main() {
   fmt.Scanln(&name)
 
   fmt.Println("name :", name)
+
+  age := 123
+  fmt.Println(age)
+
+  for i := 1; i < 5; i++ {
+    fmt.Println(i)
+  }
+
+  for age < 124 {
+    age++
+    fmt.Println("hi")
+  }
+
+  for {
+    fmt.Println("infinity")
+    if age == 124 {
+      age++
+      continue
+    }
+    break
+  }
+  
+  오종진()
+}
+
+func 오종진() {
+  fmt.Println("나는 완도사람입니다")
 }
