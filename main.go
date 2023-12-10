cf4c85c1a965089251b2cfbdfe02db298f29d108
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-12-10 23:58:52 +0900

    feat: tcp server
    책에 있는 코드 무작정 따라하기

--- main.go
+++ main.go
@@ -1,99 +1,31 @@
-package main
+package ch03
 
 import (
-	"bufio"
-	"fmt"
-	"math"
-	"os"
+	"net"
+	"testing"
 )
 
+func TestListener(t *testing.T) {
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer func() { _ = listener.Close() }()
+
+	t.Logf("bound to %q", listener.Addr())
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return err
+		}
+
+		go func(c net.Conn) {
+			defer c.Close()
+		}(conn)
+	}
+}
+
 func main() {
-  // qualification(2, 2)
-  // qualification(2, 4)
-  // qualification(2, 5)
-
-  // Defer()
-
-  // fmt.Println("hi")
-  // 패닉()
-  // fmt.Println("hello")
-
-  test := new(최형우)
-
-  fmt.Println(test)
-}
-
-func StandardIo() {
-  var f *os.File
-  f = os.Stdin
-  defer f.Close()
-
-  scanner := bufio.NewScanner(f)
-  for scanner.Scan() {
-    fmt.Println(">", scanner.Text())
-  }
-}
-
-func repeat() {
-  var name string
-
-  fmt.Scanln(&name)
-
-  fmt.Println("name :", name)
-
-  age := 123
-  fmt.Println(age)
-
-  for i := 1; i < 5; i++ {
-    fmt.Println(i)
-  }
-
-  for age < 124 {
-    age++
-    fmt.Println("hi")
-  }
-
-  for {
-    fmt.Println("infinity")
-    if age == 124 {
-      age++
-      continue
-    }
-    break
-  }
-}
-
-func qualification(a float64, b float64) {
-  if c := math.Pow(a, b); c < 10 {
-    fmt.Println("hi", c)
-  } else if c < 20 {
-    백승민(c)
-  } else {
-    오종진(c)
-  }
-}
-
-func 백승민(c float64) {
-  fmt.Println("나는 백승민이고 강진을 좋아합니다", c)
-}
-
-func 오종진(c float64) {
-  fmt.Println("나는 오종진이고 완도사람입니다", c)
-}
-
-func Defer() {
-  defer fmt.Println("World")
-  fmt.Println("Hello")
-}
-
-func 패닉() {
-  defer recover()
-  fmt.Println("전")
-  panic("으악")
-}
-
-type 최형우 struct {
-  id string
-  안경 bool
-  age int
 }
